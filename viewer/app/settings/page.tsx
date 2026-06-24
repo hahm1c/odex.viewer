547c3b44f70570b31a2f5ecd8d3cbca15882b074
Michael Hahn <michael.a.hahn@mercedes-benz.com>
2026-06-24 15:40:03 +0200

    Bump form-data and tar versions, apply npm audit fixes. Close #23. Close #24. Bump js-yaml from 4.1.1 to 4.3.0, close #25. Fix two linter warnings.
    Signed-off-by: Michael Hahn <michael.a.hahn@mercedes-benz.com>
    Approved-by: Andreas Lauser <andreas.lauser@mercedes-benz.com>

--- viewer/app/settings/page.tsx
+++ viewer/app/settings/page.tsx
@@ -4,14 +4,11 @@
 import { Button } from '@heroui/button'
 import { Tooltip } from '@heroui/tooltip'
 import clsx from 'clsx'
-import { useRouter } from 'next/navigation'
 
 import { title, subtitle } from '@/components/primitives'
 import { clearSettingsStorage } from '@/storage/settings'
 
 export default function SettingsPage() {
-  const router = useRouter()
-
   return (
     <div>
       <h1 className={title()}>Settings</h1>
